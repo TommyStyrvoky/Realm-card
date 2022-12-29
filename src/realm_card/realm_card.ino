85928d47a743852fb3e013d1dd3cddece0209e58
Tommy Styrvoky <47039842+TommyStyrvoky@users.noreply.github.com>
2022-12-28 22:37:25 -0600

    Update realm_card.ino

--- src/realm_card/realm_card.ino
+++ src/realm_card/realm_card.ino
@@ -27,9 +27,7 @@
 #define triangleWidth 20
 #define heightOffset 15
 #define textSize 2
-#define textWidth 7
-#define textHeight 7
-#define textHeightOffset (textHeight * textSize) / 2 + heightOffset
+
 #define triangleOffset 30
 #define lTriangleX 30
 #define rTriangleX 210
@@ -40,10 +38,11 @@ float frameRate = 24;
 #define maxDelay 3000
 #define touchCoolDown 3000
 #define debounce 250
-uint maxFrames 12  //anything beyond 12 frames of 240X320 overfills ram
+#define maxFramesToLoad 48  //anything beyond 12 frames of 240X320 overfills ram, this is just a placeholder for the maximum frames that could be loaded at a lower resolution
+uint maxFrames = maxFramesToLoad;
 
-  // global variables
-  uint x,
+// global variables
+uint x,
   y;
 unsigned long currentFrame, lastFrameDraw, lastTouchEvent, lastpress, delayTime, lastDelay;
 unsigned long currentDirectory = 1, selectedDirectory = 1;  //directory 0 = system
@@ -54,7 +53,6 @@ char folderList[folderListCT][strBufferSize];
 uint folderCount;
 bool uIActiveFlag, debounceFlag, delayFlag, delayAnimation = true;
 char extension[] = ".bmp";
-#define textTrunc 8
 #define SD_MHZ 25
 
 // classes
@@ -64,7 +62,7 @@ SdFile directory;
 TouchScreen ts = TouchScreen(XP, YP, XM, YM, touchscreenResistanceX);
 Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
 Adafruit_ImageReader reader(SD);
-Adafruit_Image img[maxFrames];
+Adafruit_Image img[maxFramesToLoad];
 
 
 int32_t width = 0, height = 0;
@@ -72,13 +70,20 @@ int32_t width = 0, height = 0;
 void setup() {  //first loop
   Serial.begin(115200);
   pinMode(batteryStatus, INPUT);
-  if (!SD.begin(SD_CS, SD_SCK_MHZ(SD_MHZ))) {  //throw sd error
-    tft.fillScreen(0);                         //load screen here
+  tft.begin();
+  if (!SD.begin(SD_CS, SD_SCK_MHZ(SD_MHZ))) {  //check sd card
+    tft.fillScreen(0);         
+    tft.setTextColor(ILI9341_RED);
+    tft.setTextSize(textSize);
+    drawCenteredText("SD Card",120,160-12);
+    drawCenteredText("not detected",120,160+12);
     while (true) {}                            //do nothing
   }
-  tft.begin();
   tft.fillScreen(0);  //load screen here
-
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(textSize);
+  drawCenteredText("Reading",120,160-12);
+  drawCenteredText("card.",120,160+12);
   root.open("/");
   SD.vwd()->rewind();
   while (directory.openNext(&root, O_RDONLY)) {  //read all of the directories on the sd card and store their names to memory
@@ -89,10 +94,17 @@ void setup() {  //first loop
     }
     directory.close();
   }
+  char folderCountChar[10];
+  ltoa(folderCount, folderCountChar, 10);
   tft.fillScreen(0);  //end load screen
-
+  tft.setTextColor(ILI9341_WHITE);
+  tft.setTextSize(textSize);
+  drawCenteredText("Found",120,160-24);
+  drawCenteredText(folderCountChar,120,160);
+  drawCenteredText("animations.",120,160+24);
   currentFrame = 0;
   currentDirectory = 1;
+  delay(1000);
   loadFrames();
 }
 
@@ -107,12 +119,14 @@ void loop() {                 //main
 
   if ((millis() - lastTouchEvent >= touchCoolDown) && uIActiveFlag) {  //hide UI if not active and load next frame set if changed
     uIActiveFlag = false;
-    if (currentDirectory != selectedDirectory) {
+    if (currentDirectory != selectedDirectory) {  //only load new frames if directory changed
       currentDirectory = selectedDirectory;
       currentFrame = 0;
+      loadFrames();
     }
-    loadFrames();
-    //TODO reset frame counter and set selected directory as active. Get frame count of new sequence
+    else{
+      selectedDirectory = currentDirectory;
+    }
   }
 
   if (millis() - lastpress >= debounce) {  //set debounce flag false after elapsed
@@ -164,39 +178,65 @@ void loop() {                 //main
       //draw text
       tft.setTextColor(ILI9341_WHITE);
       tft.setTextSize(textSize);
-      tft.setTextSize(2);
+      drawCenteredText( folderList[selectedDirectory], 160,heightOffset+triangleWidth/2);
+
       tft.setCursor(0, 150);
       tft.println(x);
       tft.setCursor(0, 165);
       tft.println(y);
       //TODO draw battery %
+      //TODO calculate battery viktage
     }
   }
 }
 
-
+void drawCenteredText(char* text,int px,int py){
+  int16_t x1, y1;
+  uint16_t w, h;
+  tft.getTextBounds(text, 0, 0, &x1, &y1, &w, &h);
+  tft.setCursor((px-w/2), (py-h/2));
+  tft.println(text);
+}
 
 void loadFrames() {  //loads frames into memory from sdcard
-  ImageReturnCode stat;
+                     //TODO add loading screen stuff
   char directory[strBufferSize];
   strcat(directory, "/");
   strcat(directory, folderList[currentDirectory]);
   strcat(directory, "/");
 
-  Serial.println(folderList[currentDirectory]);
-  Serial.println(currentDirectory);
+  ImageReturnCode stat;
   char buf[strBufferSize + strBufferSize + 10 + 4];
-  char frame[10];
+  char frameChar[10];
+  unsigned long framesLoaded;
 
-  for (unsigned long i = 0; i < maxFrames; i++) {  // TODO rework this to load as many images until there are no more images or ram space, and dynamically set max frames based on this
-    ltoa(i + 1, frame, 10);
+  tft.fillScreen(0);
+  Serial.print("Loading: ");
+  Serial.print(folderList[currentDirectory]);
+  Serial.println("/");
+  while (true) {  //keep loading frames if they exist or until no more memory can be allocated
+    ltoa(framesLoaded + 1, frameChar, 10);
     strcpy(buf, directory);
-    strcat(buf, frame);
+    strcat(buf, frameChar);
     strcat(buf, extension);
-    stat = reader.loadBMP(buf, img[i]);
-    Serial.println(frame);
+    Serial.print("Frame: ");
+    Serial.println(frameChar);
+    stat = reader.loadBMP(buf, img[framesLoaded]);
     reader.printStatus(stat);
+    if (stat != 0) {  //anything other than success
+      break;
+    }
+    tft.fillScreen(0);
+    tft.setTextSize(textSize);
+    drawCenteredText(folderList[currentDirectory],120,160-24);
+    drawCenteredText("loaded:",120,160);
+    drawCenteredText(frameChar,120,160+24);
+    framesLoaded++;
   }
+  if (framesLoaded >= maxFramesToLoad) {  //prevent overflow
+    framesLoaded = maxFramesToLoad;
+  }
+  maxFrames = framesLoaded;
   //TODO set framerate and Random wait logic somehow by using the directory naming structure
 }
 
@@ -204,14 +244,13 @@ void playFrame() {  //indexes through frame objects
   img[currentFrame].draw(tft, 0, 0);
   if (currentFrame >= maxFrames) {
     currentFrame = 0;
-    if (delayAnimation) {
+    if (delayAnimation) {//sets random delay at end of sequence
       delayFlag == true;
       delayTime = random(minDelay, maxDelay);
       lastDelay = millis();
     }
   }
-  Serial.println(currentFrame);
-  if (!delayFlag) {
+  if (!delayFlag) {  //prevents advance to next frame if delay is in place at end of sequence
     currentFrame++;
   }
 }
